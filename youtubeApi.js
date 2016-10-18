9c01afb046399758ae27b5cfe39aed41f9ab43c4
TOMILAYOR <TOMILAYOR>
2016-10-18 17:25:30 +0100

    Indentation edit as per Instruction

--- youtubeApi.js
+++ youtubeApi.js
@@ -2,21 +2,22 @@ function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
 }
+
 function onPageLoad() {
     gapi.client.load('youtube', 'v3', onApiLoaded);
 }
 
 function onApiLoaded() {
 	gapi.client.setApiKey('AIzaSyAupZL4G1kcKRbHVmDfgk4uSPHHHwwPDYw');
-    var requests = gapi.client.youtube.search.list({
-        part: 'snippet',
+	var requests = gapi.client.youtube.search.list({
+		part: 'snippet',
         q: 'andela',
         regionCode: 'NG'
-        
     });
-    
+
     requests.execute(searchResponse);
 }
+
 function searchResponse(response){
     showResponse(response)
 }
